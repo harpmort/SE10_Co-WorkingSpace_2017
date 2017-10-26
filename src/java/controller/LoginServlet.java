5c8cc1a7ca4c506b4a7bc1bf85c94957612c574a
Asus <Asus@DESKTOP-RQNEM7M>
2017-10-26 18:44:09 +0700

    แก้ฐานข้อมูลเพิ่มตารางและข้อมูล แก้conectionใหม่ สร้าง Member class LoginServlet

---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Member;
+
+
+/**
+ *
+ * @author Asus
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String user = request.getParameter("user");
+            String password = request.getParameter("password");
+            int check = 0;
+            ServletContext ctx = getServletContext();
+            Connection conn = (Connection) ctx.getAttribute("connection");
+            HttpSession session = request.getSession();
+            try {
+                Statement stmt = conn.createStatement();
+                String sql = "SELECT * from member where username='" + user + "'";
+                ResultSet rs = stmt.executeQuery(sql);
+                if (rs.next()) {
+                    if (password.equals(rs.getString("password"))) {
+                        Member member = new Member(conn);
+                        member.importData(user,password);
+                        session.setAttribute("member", member);
+                        check = 1;
+                        session.setAttribute("check", check);
+                        RequestDispatcher pg = request.getRequestDispatcher("index.html");
+                        pg.forward(request, response);
+                    }  else {
+                        check = 2;
+                        request.setAttribute("check", check);
+                        RequestDispatcher pg = request.getRequestDispatcher("landing.jsp");
+                        pg.forward(request, response);
+                    }
+
+                }  else {
+                    check = 3;
+                    request.setAttribute("check", check);
+                    RequestDispatcher pg = request.getRequestDispatcher("landing.jsp");
+                    pg.forward(request, response);
+                }
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/model/Member.java
+++ src/java/model/Member.java
@@ -5,10 +5,115 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author Asus
  */
 public class Member {
+    private int idmember;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private int type;
+    
+    Connection conn;
+    
+    public Member(Connection connection) {
+        conn = connection;
+    }
+    
+    public void importData(String username, String password) {
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * from member where username='" + username + "'";
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                if (password.equals(rs.getString("password"))) {
+                    firstname = (rs.getString("firstname"));
+                    lastname = (rs.getString("lastname"));
+                    email = (rs.getString("email"));
+                    phone = (rs.getString("phone"));
+                    type = (rs.getInt("type"));
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public int getIdmember() {
+        return idmember;
+    }
+
+    public void setIdmember(int idmember) {
+        this.idmember = idmember;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+    
+    
+    
     
 }
